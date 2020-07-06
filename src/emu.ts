23ea0ff0a8c758a679440cd41cd20d95f1f2d453
Joren Van Onder <joren@jvo.sh>
2020-07-05 21:16:50 -0700

    Read bytes

--- src/emu.ts
+++ src/emu.ts
@@ -1,2 +1,22 @@
-let a: number = 1;
-console.log(101);
+const inputElement = document.getElementById("rom")!;
+inputElement.addEventListener("change", handleFiles, false);
+
+// Can't use File.arrayBuffer() because it's not supported in Safari
+function readFile(file: File): Promise<ArrayBuffer> {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = function(evt) {
+      resolve(evt.target!.result as ArrayBuffer);
+    };
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+async function handleFiles(event: Event) {
+  const target = event.target as HTMLInputElement;
+  const rom = target.files![0];
+  console.log(rom);
+
+  const bytes = await readFile(rom);
+  console.log(bytes);
+}
