600d6f61c4ce7ec76521728abc5b077816ca7999
Joren Van Onder <joren@jvo.sh>
2020-07-10 08:58:05 -0700

    Implement some instructions

--- src/cpu.ts
+++ src/cpu.ts
@@ -27,6 +27,16 @@ export class CPU {
     return (byte >> from) & mask;
   }
 
+  binString(x: number): string {
+    let bin = x.toString(2);
+    bin = bin.padStart(8, "0");
+    return `0b${bin}`;
+  }
+
+  logUnknownInstruction(byte: number) {
+    console.log(`unknown instruction ${this.binString(byte)}`);
+  }
+
   // 4 msb between [1001, 1111] (except HALT: 0x76)
   run() {
     console.log(this.memory.length);
@@ -36,23 +46,60 @@ export class CPU {
         break;
       }
 
-      const byte = this.memory[this.PC];
+      const byte = this.memory[this.PC++];
 
-      if (byte === 0x00) {
-        console.log("No-op");
-        this.PC += 1;
-      } else {
-        const twoMSBs = this.getBits(byte, 7, 8);
-        if (twoMSBs == 0b01) {
+      const twoMSBs = this.getBits(byte, 7, 8);
+      switch (twoMSBs) {
+        case 0b00:
+          if (byte === 0) {
+            console.log("No-op");
+          } else if (byte === 0b00110110) {
+            // LD (HL), n
+            const immediate = this.memory[this.PC++];
+            console.log(
+              `Load to the address specified in HL the immediate ${immediate}`
+            );
+          } else if (byte === 0b00001010) {
+            // LD A, (BC)
+            console.log(
+              "Load to the 8-bit A register, data from the absolute address specified by the 16-bit register BC."
+            );
+          } else if (byte === 0b00011010) {
+            // LD A, (DE)
+            console.log(
+              "Load to the 8-bit A register, data from the absolute address specified by the 16-bit register DE."
+            );
+          } else if (this.getBits(byte, 0, 2) === 0b110) {
+            const immediate = this.memory[this.PC++];
+            const registerA = this.getBits(byte, 3, 5);
+
+            // LD r, n
+            console.log(`copy immediate ${immediate} into ${registerA}`);
+          } else {
+            this.logUnknownInstruction(byte);
+          }
+          break;
+        case 0b01:
           const registerA = this.getBits(byte, 3, 5);
           const registerB = this.getBits(byte, 0, 2);
 
-          console.log(`copy ${registerA} -> ${registerB}`);
-        } else {
-          console.log("Unknown instruction");
-        }
-
-        this.PC += 1;
+          if (registerB === 0b110) {
+            // LD r, (HL)
+            console.log(
+              `copy from the address specified in 16 bit register HL into ${registerA}`
+            );
+          } else if (this.getBits(byte, 3, 7) === 0b01110) {
+            // LD (HL), r
+            console.log(
+              `Copy the 8 bit value from register ${registerB} into the address specified in HL`
+            );
+          } else {
+            // LD r, r’
+            console.log(`copy ${registerA} -> ${registerB}`);
+          }
+          break;
+        default:
+          this.logUnknownInstruction(byte);
       }
     }
   }
