ba62dda242f29509664dbcf0deecd5601de4bf6b
Joren Van Onder <joren@jvo.sh>
2020-09-13 15:16:12 -0700

    Allow pausing at any point

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,9 @@ function runEmulator(bytes: Uint8Array) {
   const next = document.getElementById("next")!;
   next.addEventListener("click", () => emu.run());
 
+  const pause = document.getElementById("pause")!;
+  pause.addEventListener("click", () => (emu.paused = true));
+
   emu.run();
 }
 
