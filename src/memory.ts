2095d7d9054c2775646f32f39512915728281671
Joren Van Onder <joren@jvo.sh>
2020-10-03 15:49:36 -0700

    Always indicate the screen frame is ready
    To get the bootrom to load without a graphics subsystem.

--- src/memory.ts
+++ src/memory.ts
@@ -4,6 +4,9 @@ export class Memory {
   constructor(rom: Uint8Array) {
     this.bytes = new Uint8Array(0xffff);
     this.bytes.set(rom, 0);
+
+    // hacks
+    this.bytes[0xff44] = 0x90; // indicate screen frame is done
   }
 
   getSize() {
