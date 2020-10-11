865ce728d675abea2aa63d31d94b44f6ecaba09f
Joren Van Onder <joren@jvo.sh>
2020-10-11 14:12:21 -0700

    Test 4 tiles

--- src/video.spec.ts
+++ src/video.spec.ts
@@ -7,6 +7,15 @@ import { Video } from "./video";
 
 describe("Video subsystem", function () {
   it("should correctly decode tiles", function () {
+    // +----------+----------+
+    // |          |          |
+    // |  TILE 1  |  TILE 2  |
+    // |          |          |
+    // +----------+----------+
+    // |          |          |
+    // |  TILE 3  |  TILE 4  |
+    // |          |          |
+    // +----------+----------+
     const memory = new Memory(new Uint8Array([
       0x5f, 0x3f, 0x3f, 0x5f, // tile 1 row 1 & 2
       0xff, 0xff, 0xff, 0xff, // tile 1 row 3 & 4
@@ -16,44 +25,66 @@ describe("Video subsystem", function () {
       0xff, 0xff, 0xff, 0xff, // tile 2 row 3 & 4
       0xff, 0xff, 0xff, 0xff, // tile 2 row 5 & 6
       0xff, 0xff, 0xff, 0xff, // tile 2 row 7 & 8
+      0x5f, 0x3f, 0x3f, 0x5f, // tile 3 row 1 & 2
+      0xff, 0xff, 0xff, 0xff, // tile 3 row 3 & 4
+      0xff, 0xff, 0xff, 0xff, // tile 3 row 5 & 6
+      0xff, 0xff, 0xff, 0xff, // tile 3 row 7 & 8
+      0xff, 0xff, 0xff, 0xff, // tile 4 row 1 & 2
+      0xff, 0xff, 0xff, 0xff, // tile 4 row 3 & 4
+      0xff, 0xff, 0xff, 0xff, // tile 4 row 5 & 6
+      0xff, 0xff, 0xff, 0xff, // tile 4 row 7 & 8
     ]));
 
     const { document } = new JSDOM("<canvas>").window;
     const canvas = document.getElementsByTagName("canvas")[0];
     const video = new Video(memory, canvas);
 
-    const image = canvas.getContext("2d")!.createImageData(8, 16);
+    const image = canvas.getContext("2d")!.createImageData(16, 16);
     for (let i = 0; i < image.data.length; i++) {
       assert.equal(image.data[i], 0);
     }
 
     const colorMap = video.getColorMap();
 
-    const expectedColorsTile = Array(2 * 8 * 8).fill(colorMap[3]); // fill with black (default of canvas)
+    const expectedColorsTile = Array(4 * 8 * 8).fill(colorMap[3]); // fill with black (default of canvas)
     // tile 1 first row
     expectedColorsTile[0] = colorMap[0];
     expectedColorsTile[1] = colorMap[1];
     expectedColorsTile[2] = colorMap[2];
 
-    // tile 1 second row
-    expectedColorsTile[8] = colorMap[0];
-    expectedColorsTile[9] = colorMap[2];
-    expectedColorsTile[10] = colorMap[1];
-
     // tile 2 first row
-    expectedColorsTile[64] = colorMap[0];
-    expectedColorsTile[65] = colorMap[1];
-    expectedColorsTile[66] = colorMap[2];
+    expectedColorsTile[8] = colorMap[0];
+    expectedColorsTile[9] = colorMap[1];
+    expectedColorsTile[10] = colorMap[2];
 
-    expectedColorsTile[72] = colorMap[0];
-    expectedColorsTile[73] = colorMap[2];
-    expectedColorsTile[74] = colorMap[1];
+    // tile 1 second row
+    expectedColorsTile[16] = colorMap[0];
+    expectedColorsTile[17] = colorMap[2];
+    expectedColorsTile[18] = colorMap[1];
+
+    // tile 2 second row
+    expectedColorsTile[24] = colorMap[0];
+    expectedColorsTile[25] = colorMap[2];
+    expectedColorsTile[26] = colorMap[1];
+
+    // tile 3 first row
+    expectedColorsTile[128] = colorMap[0];
+    expectedColorsTile[129] = colorMap[1];
+    expectedColorsTile[130] = colorMap[2];
+
+    // tile 3 second row
+    expectedColorsTile[144] = colorMap[0];
+    expectedColorsTile[145] = colorMap[2];
+    expectedColorsTile[146] = colorMap[1];
 
     video.renderTile(image, 0, 0, 0);
     video.renderTile(image, 16, 0, 8);
+    video.renderTile(image, 32, 8, 0);
+    video.renderTile(image, 48, 8, 8);
 
+    let colorIndex = 0;
     let offset = 0;
-    for (const [colorIndex, colorExpected] of expectedColorsTile.entries()) {
+    for (const colorExpected of expectedColorsTile) {
       for (let i = 0; i < 4; i++) {
         assert.equal(
           image.data[offset + i],
@@ -66,6 +97,7 @@ describe("Video subsystem", function () {
       }
 
       offset += 4;
+      colorIndex++;
     }
 
     // first row
