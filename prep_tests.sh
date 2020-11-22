3275f76de11598f0dd83d7f9f66be51ae3e20ab7
Joren Van Onder <joren@jvo.sh>
2020-11-22 17:22:39 -0500

    Support multiline import statements in prep_tests.sh
    E.g.
    
    import {
      View,
      RegisterView,
      SPView,
      BankView,
      MemoryView,
      StackView,
      PauseButton,
      RunBootRomButton,
      StepNextButton,
      CopyButton,
      BreakpointSetter
    } from "./views.js";

--- prep_tests.sh
+++ prep_tests.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 cp -r $PWD/src $PWD/test_src
-sed -ie 's/^\(import.*\)"\(.*\)\.js"/\1"\2"/g' $PWD/test_src/*.ts
+sed -ie 's/\( from .*\)"\(.*\)\.js"/\1"\2"/g' $PWD/test_src/*.ts
