fa5ea930d488527f64ffbac050207356b1bbb6d1
Joren Van Onder <joren@jvo.sh>
2020-11-22 18:16:06 -0500

    Adapt cpu tests

--- src/cpu.spec.ts
+++ src/cpu.spec.ts
@@ -1,20 +1,25 @@
 import "mocha";
 import * as assert from "assert";
 import { CPU } from "./cpu";
+import { ControllerMock } from "./controller";
 
 describe("flags", function() {
   it("should correctly set the zero flag", function() {
-    const cpu = new CPU(new Map());
-    assert.equal(cpu.regs[0x6], 0);
+    const controller = new ControllerMock();
+    const cpu = new CPU();
+    cpu.setController(controller);
+    assert.equal(cpu.getReg(0x6), 0);
 
     cpu.setZeroFlag(1);
-    assert.equal(cpu.regs[0x6], 0b1000_0000);
+    assert.equal(cpu.getReg(0x6), 0b1000_0000);
     cpu.setZeroFlag(0);
-    assert.equal(cpu.regs[0x6], 0);
+    assert.equal(cpu.getReg(0x6), 0);
   });
 
   it("should correctly set the half carry bit", function() {
-    const cpu = new CPU(new Map());
+    const controller = new ControllerMock();
+    const cpu = new CPU();
+    cpu.setController(controller);
     assert.equal(cpu.getHalfCarryFlag(), 0);
 
     cpu.setHalfCarryFlagAdd(0b1000, 0b0100);
